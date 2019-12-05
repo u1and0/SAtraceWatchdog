ea1c628978a7d7ebf6dd68c06ffc9b0e3ec436f7
u1and0 <e01.ando60@gmail.com>
2019-12-05 23:26:27 +0900

    [mod] run loop @watchgraph.py

--- watchgraph.py
+++ watchgraph.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """dataディレクトリ下のtxtを60secごとに監視して、グラフ化したものをpng出力する"""
-# from time import sleep
+from time import sleep
 from glob import iglob
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -18,20 +18,20 @@ sns.set(style='whitegrid',
             'image.cmap': 'viridis'
         })
 
-# while True:
-#     sleep(10)
-datadir = 'data/'
-txts = {Path(i).stem for i in iglob(datadir + '*.txt')}
-pngs = {Path(i).stem for i in iglob(datadir + '*.png')}
-for base in txts - pngs:
-    df = pd.read_csv(datadir + base + '.txt',
-                     sep='\s+',
-                     index_col=0,
-                     skiprows=1,
-                     skipfooter=1,
-                     names=['Min', 'Mean', 'Max'],
-                     engine='python')
-    df.Mean.plot()
-    plt.savefig(datadir + base + '.png')
-    plt.close()  # reset plot
-    print(f'Succeeded export image {datadir}{base}.png')
+while True:
+    sleep(10)
+    datadir = 'data/'
+    txts = {Path(i).stem for i in iglob(datadir + '*.txt')}
+    pngs = {Path(i).stem for i in iglob(datadir + '*.png')}
+    for base in txts - pngs:
+        df = pd.read_csv(datadir + base + '.txt',
+                         sep='\s+',
+                         index_col=0,
+                         skiprows=1,
+                         skipfooter=1,
+                         names=['Min', 'Mean', 'Max'],
+                         engine='python')
+        df.Mean.plot()
+        plt.savefig(datadir + base + '.png')
+        plt.close()  # reset plot
+        print(f'Succeeded export image {datadir}{base}.png')
